d049f4e526fe64ec9d38151040cb7176836d0fa1
Jethro-Canlas <jethrocanlas7@gmail.com>
2025-09-24 16:16:41 +0800

    mongoose

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,22 @@
+const mongoose = require('mongoose');
+const cors = require('cors');
 const express = require('express');
 const app = express();
 
 app.use(express.json());
+app.use(cors());
+
+mongoose.connect('mongodb://localhost:27017/mydatabase', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', () => {
+    console.log('Connected to MongoDB');
+});
+
 
 app.get('/', (req, res) => {
     res.send('My New App!');
